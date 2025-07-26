17627b06f5869fa264722c094ad336151934e485
chaewon9999 <w0729@naver.com>
2025-07-26 21:44:25 +0900

    refactor: h2 테스트용 로직 제거

--- src/main/java/org/example/dailydiary/common/config/SecurityConfig.java
+++ src/main/java/org/example/dailydiary/common/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,16 +30,13 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			//.csrf(AbstractHttpConfigurer::disable)
-			.csrf(csrf -> csrf.disable())  // CSRF 완전 비활성화
+			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/user/signup", "/user/login", "/auth/reissue", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-				.requestMatchers("/h2-console/**").permitAll()  // H2 Console 허용
 				.anyRequest().authenticated()
 			)
-			.headers(headers -> headers.disable()) // 모든 헤더 비활성화
 			.logout(logout -> logout
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/")
